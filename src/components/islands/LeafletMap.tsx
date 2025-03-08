4adf8901e3f0463eff91ef63d704e78e1ab29ce1
Andrew Betts <andrew@trib.tv>
2025-03-08 12:10:39 +0000

    Map tile proxy

--- src/components/islands/LeafletMap.tsx
+++ src/components/islands/LeafletMap.tsx
@@ -5,7 +5,7 @@ const LeafletMap = (props) => {
     return (
         <MapContainer center={props.center} zoom={props.zoom} scrollWheelZoom={false} zoomControl={false} dragging={false}>
             <TileLayer
-                url="https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png"
+                url="/tiles/stamen_toner/{z}/{x}/{y}{r}.png"
                 attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
         </MapContainer>
