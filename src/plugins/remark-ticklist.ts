e00b9295e2e894dda4fceed7f2b986056434badc
Andrew Betts <andrew@trib.tv>
2025-05-13 13:49:36 +0100

    Link hover, add missing file

---
+++ src/plugins/remark-ticklist.ts
@@ -0,0 +1,23 @@
+import type { ListItem } from "mdast";
+import type { RemarkPlugin } from "@astrojs/markdown-remark";
+import { visit } from "unist-util-visit";
+import { u } from "unist-builder";
+
+export const remarkTickList: RemarkPlugin = () => {
+  return (tree) => {
+    visit(tree, "listItem", (node: ListItem, idx, parent) => {
+      if (typeof node.checked !== "boolean") return;
+      console.log(node.children[0]);
+
+      const tickbox = u("html", `<span class='tickbox ${node.checked ? 'checked' : ''}'></span>`);
+
+      // Prepend the tickbox to the existing children
+      node.children.unshift(tickbox);
+
+      // Remove the `checked` property so the renderer doesn't add a checkbox
+      delete node.checked;
+
+      parent.data = { hProperties: { class: "tickbox-list" } };
+    });
+  };
+};
